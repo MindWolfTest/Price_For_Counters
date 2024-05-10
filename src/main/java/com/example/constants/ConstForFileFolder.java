e39f65998f193bb2ac6b6bf87c0f106fcba409c0
Ivan Ivanov <test0test1011000@gmail.com>
2024-05-10 20:46:09 +0300

    Fix second try

--- src/main/java/com/example/constants/ConstForFileFolder.java
+++ src/main/java/com/example/constants/ConstForFileFolder.java
@@ -2,5 +2,5 @@ package com.example.constants;
 
 public class ConstForFileFolder
 {
-    public static final String FILE_FOLDER = "C:\\Users\\Test\\Documents\\Java_Projects\\Price_For_Counters\\my.xlsx";
+    public static final String FILE_FOLDER = "my.xlsx";
 }
